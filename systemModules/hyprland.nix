ec9631428b882ad7402b4825a95c7a2ad983c915
anthonyprime202 <anthonyprime202@gmail.com>
2025-06-11 17:42:04 +0530

    Minor update to hyprland system config

--- systemModules/hyprland.nix
+++ systemModules/hyprland.nix
@@ -34,7 +34,7 @@
       gnome-calculator
       gnome-disk-utility
       nemo
-      sww
+      swww
       loupe
 
       wlinhibit
@@ -45,6 +45,7 @@
 
     nix.settings = {
       substituters = ["https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
   });
